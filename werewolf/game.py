f30787bcd584daf37d0f99ede819d748acedbe4f
Feiyang Chen <feiyangc@google.com>
2024-06-21 23:01:27 +0000

    Add license headers

--- werewolf/game.py
+++ werewolf/game.py
@@ -1,3 +1,17 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Werewolf game."""
 
 from collections import Counter
@@ -7,7 +21,6 @@ from typing import List
 
 import tqdm
 
-from werewolf.model import Doctor, Player, Seer, Villager, Werewolf
 from werewolf.model import Round, RoundLog, State, VoteLog
 from werewolf.config import  MAX_DEBATE_TURNS, RUN_SYNTHETIC_VOTES
 
